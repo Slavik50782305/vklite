cdd6b9d2c7f6efed48a8a4fd3f544d223b7a873f
Slavik50782305 <barabanovss27@gmail.com>
2026-09-14 14:11:20 +0500

    Version 0.7 cards

--- app/src/main/java/com/vklite/FeedAdapter.kt
+++ app/src/main/java/com/vklite/FeedAdapter.kt
@@ -1,5 +1,7 @@
 package com.vklite
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -8,20 +10,29 @@ class FeedAdapter(
     private val posts: List<Post>
 ) : RecyclerView.Adapter<FeedAdapter.PostViewHolder>() {
 
-    class PostViewHolder(
-        val textView: TextView
-    ) : RecyclerView.ViewHolder(textView)
+    class PostViewHolder(view: View) :
+        RecyclerView.ViewHolder(view) {
+
+        val author: TextView =
+            view.findViewById(R.id.postAuthor)
+
+        val text: TextView =
+            view.findViewById(R.id.postText)
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): PostViewHolder {
 
-        val tv = TextView(parent.context)
-        tv.textSize = 18f
-        tv.setPadding(32, 32, 32, 32)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(
+                R.layout.item_post,
+                parent,
+                false
+            )
 
-        return PostViewHolder(tv)
+        return PostViewHolder(view)
     }
 
     override fun onBindViewHolder(
@@ -30,8 +41,8 @@ class FeedAdapter(
     ) {
         val post = posts[position]
 
-        holder.textView.text =
-            "${post.author}\n\n${post.text}"
+        holder.author.text = post.author
+        holder.text.text = post.text
     }
 
     override fun getItemCount() = posts.size
